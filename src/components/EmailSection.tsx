a429d541f5ca63fb06c5335b1fb302599d3cfe75
Hannah-C <90980138+Hannah-C@users.noreply.github.com>
2024-01-15 20:38:53 -0500

    change

--- src/components/EmailSection.tsx
+++ src/components/EmailSection.tsx
@@ -49,7 +49,7 @@ const EmailSection = () => {
       <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-purple-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-2/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
       <div className="z-10">
         <h5 className="text-xl font-bold text-white my-2">
-          Let&apos;s Connect
+          Let's Connect
         </h5>
         <p className="text-hannahs-text mb-4 max-w-md">
           {" "}
